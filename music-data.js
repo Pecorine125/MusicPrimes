25de8b68db6a25596be5b4943e32b218922a79a5
Pecorine <harahellima@gmail.com>
2025-08-12 14:29:38 -0300

    asd

--- music-data.js
+++ music-data.js
@@ -3,23 +3,23 @@ const musicFolders = {
     "anime-br": {
         path: "public/music-anime-br/",
         files: [
-            { file: "musica1.mp4", title: "Abertura One Piece", cover: "public/covers/anime.jpg" },
-            { file: "musica2.mp4", title: "Abertura Naruto", cover: "public/covers/anime.jpg" }, // mesma capa
-            { file: "musica3.mp4", title: "Abertura Dragon Ball", cover: "public/covers/dragonball.jpg" }
+            { file: "", title: "", cover: "" },
+            { file: "", title: "", cover: "" }, // mesma capa
+            { file: "", title: "", cover: "" }
         ]
     },
     "anime-jp": {
         path: "public/music-anime-jp/",
         files: [
-            { file: "musica1.ogg", title: "Tema de Attack on Titan", cover: "public/covers/aot.jpg" },
-            { file: "musica2.ogg", title: "Tema de Demon Slayer", cover: "public/covers/aot.jpg" } // mesma capa
+            { file: "", title: "", cover: "" },
+            { file: "", title: "", cover: "" } // mesma capa
         ]
     },
     "tempo-inovar": {
         path: "public/music-tempo-inovar/",
         files: [
-            { file: "musica1.mp4", title: "Inovar Mix 1", cover: "public/covers/mix.jpg" },
-            { file: "musica2.mp4", title: "Inovar Mix 2", cover: "public/covers/mix.jpg" } // mesma capa
+            { file: "", title: "", cover: "" },
+            { file: "", title: "", cover: "" } // mesma capa
         ]
     }
 };
