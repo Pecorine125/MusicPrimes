7ca2d9f250914a4638a83024d90b1a461f74a071
SeuNome <seu_email@example.com>
2025-08-12 18:23:43 +0000

    Atualização automática das músicas via Colab

--- music-data.js
+++ music-data.js
@@ -1,37 +1,73 @@
+// asd
 const musicList = [
-  { 
-    title: "", 
-    file: "", 
-    cover: "" 
-  },
-  { 
-    title: "", 
-    file: "", 
-    cover: "" 
-  },
-  { 
-    title: "", 
-    file: "", 
-    cover: "" 
-  },
-  { 
-    title: "", 
-    file: "", 
-    cover: "" 
-  },
-  { 
-    title: "", 
-    file: "", 
-    cover: "" 
-  },
-  { 
-    title: "", 
-    file: "", 
-    cover: "" 
-  },
-  { 
-    title: "", 
-    file: "", 
-    cover: "" 
-  }
+    {
+        "title": "",
+        "file": "",
+        "cover": ""
+    },
+    {
+        "title": "",
+        "file": "",
+        "cover": ""
+    },
+    {
+        "title": "",
+        "file": "",
+        "cover": ""
+    },
+    {
+        "title": "",
+        "file": "",
+        "cover": ""
+    },
+    {
+        "title": "",
+        "file": "",
+        "cover": ""
+    },
+    {
+        "title": "",
+        "file": "",
+        "cover": ""
+    },
+    {
+        "title": "",
+        "file": "",
+        "cover": ""
+    },
+    {
+        "title": "Musica1",
+        "file": "music-tempo-inovar/Musica1.mp3",
+        "cover": "covers/Capa1.jpg"
+    },
+    {
+        "title": "Musica2",
+        "file": "music-tempo-inovar/Musica2.mp3",
+        "cover": "covers/Capa2.jpg"
+    },
+    {
+        "title": "Musica3",
+        "file": "music-tempo-inovar/Musica3.mp3",
+        "cover": "covers/Capa3.jpg"
+    },
+    {
+        "title": "Musica4",
+        "file": "music-tempo-inovar/Musica4.mp3",
+        "cover": "covers/Capa4.jpg"
+    },
+    {
+        "title": "Musica5",
+        "file": "music-tempo-inovar/Musica5.mp3",
+        "cover": "covers/Capa5.jpg"
+    },
+    {
+        "title": "Musica6",
+        "file": "music-tempo-inovar/Musica6.mp3",
+        "cover": "covers/Capa6.jpg"
+    },
+    {
+        "title": "Musica7",
+        "file": "music-tempo-inovar/Musica7.mp3",
+        "cover": "covers/Capa7.jpg"
+    }
 ];
