1a137b2e72e7e5f6bf6c17dc01b7350e68bdae6b
Pecorine <harahellima@gmail.com>
2025-08-12 14:19:25 -0300

    Create music-data.js

---
+++ music-data.js
@@ -0,0 +1,25 @@
+// Lista de músicas
+const musicFolders = {
+    "anime-br": {
+        path: "public/music-anime-br/",
+        files: [
+            { file: "musica1.mp4", title: "Abertura One Piece" },
+            { file: "musica2.mp4", title: "Abertura Naruto" },
+            { file: "musica3.mp4", title: "Abertura Dragon Ball" }
+        ]
+    },
+    "anime-jp": {
+        path: "public/music-anime-jp/",
+        files: [
+            { file: "musica1.ogg", title: "Tema de Attack on Titan" },
+            { file: "musica2.ogg", title: "Tema de Demon Slayer" }
+        ]
+    },
+    "tempo-inovar": {
+        path: "public/music-tempo-inovar/",
+        files: [
+            { file: "musica1.mp4", title: "Inovar Mix 1" },
+            { file: "musica2.mp4", title: "Inovar Mix 2" }
+        ]
+    }
+};
