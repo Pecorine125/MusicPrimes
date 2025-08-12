4b3ed9fac5e07c8d6fcba6d5484087e7f060123f
Pecorine <harahellima@gmail.com>
2025-08-12 15:33:41 -0300

    asdasd

--- music-data.js
+++ music-data.js
@@ -3,36 +3,36 @@ const musicList = [
     {
         "title": "Musica1",
         "file": "music-tempo-inovar/Musica1.mp3",
-        "cover": "covers/Capa1.jpg"
+        "cover": "public/public/covers/Capa1.jpg"
     },
     {
         "title": "Musica2",
         "file": "music-tempo-inovar/Musica2.mp3",
-        "cover": "covers/Capa2.jpg"
+        "cover": "public/covers/Capa2.jpg"
     },
     {
         "title": "Musica3",
         "file": "music-tempo-inovar/Musica3.mp3",
-        "cover": "covers/Capa3.jpg"
+        "cover": "public/covers/Capa3.jpg"
     },
     {
         "title": "Musica4",
         "file": "music-tempo-inovar/Musica4.mp3",
-        "cover": "covers/Capa4.jpg"
+        "cover": "public/covers/Capa4.jpg"
     },
     {
         "title": "Musica5",
         "file": "music-tempo-inovar/Musica5.mp3",
-        "cover": "covers/Capa5.jpg"
+        "cover": "public/covers/Capa5.jpg"
     },
     {
         "title": "Musica6",
         "file": "music-tempo-inovar/Musica6.mp3",
-        "cover": "covers/Capa6.jpg"
+        "cover": "public/covers/Capa6.jpg"
     },
     {
         "title": "Musica7",
         "file": "music-tempo-inovar/Musica7.mp3",
-        "cover": "covers/Capa7.jpg"
+        "cover": "public/covers/Capa7.jpg"
     }
 ];
